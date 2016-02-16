68adb62816a34010a59faf3d1c014f20c4609b81
Alistair Duggin <alistair.duggin@btinternet.com>
2016-02-16 23:20:43 +0100

    Add more examples and fix validation issues

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(function() {
     $('#webchat').click(function(){
         window.open('demo.html','webchat', 'width=200, height=200');
     });
+
+    $('a.trap').keydown(function(event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+        var text = $(this).text();
+        window.open(href, text);
+    });
+
 });
