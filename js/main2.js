b733088df34c00c9bf377d92cffe790435627edd
Mehmet Duran <mehmet.duran@gmail.com>
2016-07-20 15:07:20 +0100

    Four new lightbox examples have been added
    1) lightbox with focus areas within it, but when the lightbox opens focus
    remains in the background page area requiring the user to tab through a
    number of links before getting to the focus areas within the light box.
    
    2) A lightbox where the X doesn't receive keyboard focus but it can be closed
    by clicking on it with a mouse or touching using a touch screen.
    
    3) Focus moves to a focus area in the lightbox, but focus is not retained
    within the lightbox, ie tabbing will take the user to other focus areas on the
    page
    
    4) lightbox can't be closed using the escape key (this may not be a
    requirement but it is a commonly known way of closing light boxes)

--- js/main2.js
+++ js/main2.js
@@ -11,13 +11,46 @@ $(function (){
 
     $('.open-lightbox-close-button').click(function (event){
         event.preventDefault();
-        $('.lightbox.close-button').removeClass('hidden');
+        var $lClose = $('.lightbox.close-button');
+        $lClose.removeClass('hidden');
+        $lClose.focus();
     });
 
     $('.lightbox.close-button .close-button').click(function (){
         event.preventDefault();
        $('.lightbox.close-button').addClass('hidden'); 
     });
+
+    $('.open-lightbox-focus-far').click(function (event){
+        event.preventDefault();
+        var $lbox = $('.lightbox.focus-far');
+        $('body').append($lbox);
+        $lbox.removeClass('hidden');
+    });
+
+    $('.lightbox.focus-far .close-button').click(function (){
+        event.preventDefault();
+       $('.lightbox.focus-far').addClass('hidden'); 
+    });
+
+    $('.open-lightbox-unescapable').click(function (event){
+        event.preventDefault();
+        $('.lightbox.unescapable').removeClass('hidden');
+    });
+
+    $('.lightbox.unescapable .close-button').click(function (){
+        event.preventDefault();
+       $('.lightbox.unescapable').addClass('hidden'); 
+    });
+
+    // escape key to close lightboxes
+    $(window).keyup(function (e){
+        if (e.keyCode == 27) { //escape key
+            $('.lightbox').not('.unescapable').addClass('hidden');
+        }
+    });
+
+
 
 
 
