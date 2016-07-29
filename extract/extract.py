ec82d6ea72c0e7deaa37c4d077c348d3ea1903e3
Mehmet Duran <mehmet.duran@gmail.com>
2016-07-29 17:57:37 +0100

    Tidying up and adding the results in

--- extract/extract.py
+++ extract/extract.py
@@ -1,10 +1,20 @@
 import json
+import re
 from itertools import chain
 
 from bs4 import BeautifulSoup
 
 with open('../index.html') as indexfile:
-    html = indexfile.read()
+    originalhtml = indexfile.read()
+
+    indexfile.seek(0)
+    html_lines = indexfile.readlines()
+    for idx, line in enumerate(html_lines):
+        if "pearls" in line:
+            html_lines[idx] = "<!-- %s -->" % line
+
+    html = "".join(html_lines)
+
     indexfile.close()
 
 soup = BeautifulSoup(html, 'html.parser')
@@ -30,6 +40,11 @@ for cur in soup.find('h2', id="content").previous_sibling.next_siblings:
         tests[group][testname]['example'] = cur.renderContents()
 
 
+# Fixing the test we have to comment out
+thatTest = tests["HTML"]["Start and close tags don't match"]['example']
+tests["HTML"]["Start and close tags don't match"]['example'] = thatTest.replace('<!--', '').replace('-->', '')
+
+
 with open('../tools.html') as toolfile:
     html = toolfile.read()
     toolfile.close()
@@ -44,9 +59,15 @@ tools = ["google", "tenon", "wave", "codesniffer", "axe", "tanaguru", "sortsite"
 
 for result in results:
     title = result.findChild('th').text
-    resobj = tests[titlemap[title]][title]['results']
-    for toolname in tools:
-        resobj[toolname] = (set(result.findChild("td", class_=toolname)['class']) - set(tools)).pop()
+    if title not in titlemap:
+        # If you see these errors, it might be because you have to comment out 
+        # the example for the test titled "Start and close tags don't match"
+        # the test is testing for broken HTML, it breaks the parser
+        print "not in index: %s" % title
+    else:
+        resobj = tests[titlemap[title]][title]['results']
+        for toolname in tools:
+            resobj[toolname] = (set(result.findChild("td", class_=toolname)['class']) - set(tools)).pop()
 
 with open('tests.json', 'w') as outfile:
     json.dump(tests, outfile, indent=4)
